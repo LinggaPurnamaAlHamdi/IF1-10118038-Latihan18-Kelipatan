1d168183937f2128ed86744749e8a29326638d6f
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-10-09 22:25:54 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan18-Kelipatan/src/if1/pkg10118038/latihan18/kelipatan/IF110118038Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan18.kelipatan;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : program ini berisi program yang akan menampilkan Kelipatan
+ */
+public class IF110118038Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float bilangan = 3.5f;
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(bilangan*i);
+        }
+    }
+    
+}
